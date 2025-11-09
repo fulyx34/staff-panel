7e8d444a5ee4b6bbd3c012a6fef4a313008c1a61
Maxen <maxen@staff-panel.com>
2025-11-09 13:32:38 +0100

    Fix: Utiliser PORT dynamique pour Render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
